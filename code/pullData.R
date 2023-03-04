92be3d1d441e08567f3c111089033fe687e369a6
Rob Gordon <rfgordonjr@gmail.com>
2023-03-04 01:07:48 -0500

    Finished.

--- code/pullData.R
+++ code/pullData.R
@@ -7,6 +7,8 @@ library(polite)
 library(rvest)
 library(purrr)
 library(stringr)
+library(ggplot2)
+library(openxlsx)
 
 ## read all-nba from https://www.basketball-reference.com/awards/all_league.html ####
 all_league <- readr::read_csv(file = here::here('data/all_nba_bball_reference.csv')) %>% 
@@ -17,9 +19,11 @@ all_league_long <- all_league %>%
   select(-col) %>% 
   mutate(season = as.numeric(str_sub(season, 1, 4)) + 1,
          position = if_else(season >= 1956, str_sub(player, nchar(player), nchar(player)), NA_character_),
-         player = str_sub(player, 1, nchar(player) - 2),
+         player = if_else(season >= 1956, str_sub(player, 1, nchar(player) - 2), player),
          player = case_when(season == 1952 & grepl(pattern = "Davies", x = player, ignore.case=TRUE) ~ "Bob Davies/Dolph Schayes",
-                            TRUE ~ player)) %>% 
+                            season == 1971 & grepl(pattern = "Zelmo Beaty C Dan Issel C", x = player, ignore.case=TRUE) ~ "Zelmo Beaty/Dan Issel",
+                            TRUE ~ player),
+         position = if_else(season == 1971 & grepl(pattern = "Zelmo Beaty/Dan Issel", x = player, ignore.case=TRUE), "C", position)) %>% 
   separate_rows(player, sep = "/") %>% 
   arrange(desc(season), tm) 
 
@@ -37,18 +41,60 @@ all_star_by_year <- all_stars %>%
   mutate(selections_c = trimws(selections_c)) %>% 
   separate(selections_c, into = c("start", "end"), sep = "–", convert = TRUE) %>% 
   mutate(end = if_else(is.na(end), start, end),
-         all_star_year = map2(start, end, seq)
+         season = map2(start, end, seq)
   ) %>% 
-  unnest(all_star_year) %>%
+  unnest(season) %>%
   # mutate(player = str_replace_all(player, "[^[:alnum:]]", "")) %>%
   mutate(player = str_replace_all(player, "\\*\\[a\\]", ""),
+         player = str_replace_all(player, "\\*\\[b\\]", ""),
          player = str_replace_all(player, "\\^", ""),
          player = str_replace_all(player, "\\*", ""),
          player = str_replace_all(player, "\\†", ""),
          player = str_replace_all(player, "\\†", ""),
-         player = str_replace_all(player, "\\§", "")
-         ) 
+         player = str_replace_all(player, "\\§", ""),
+         player = case_when(grepl("Archibald", player, ignore.case=TRUE) ~ "Tiny Archibald",
+                            grepl("Penny Hardaway", player, ignore.case=TRUE) ~ "Anfernee Hardaway",
+                            TRUE ~ player)
+         ) %>% 
+  arrange(desc(season))
+# all_star_by_year %>% filter(grepl("Archibald", player, ignore.case=TRUE))
 # all_star_grouped <- all_star_by_year %>% 
 #   group_by(player) %>% 
 #   summarise(number = n_distinct(all_star_year)) %>% 
 #   ungroup()
+
+## join data to answer which all nba teams werent all stars ####
+min(all_star_by_year$all_star_year) # 1951
+min(all_league_long$season) # 1947
+all_league_not_all_stars <- all_league_long %>% 
+  filter(season >= 1951, lg == "NBA") %>% 
+  anti_join(all_star_by_year, by = c("season", "player")) 
+head(all_league_not_all_stars)
+all_star_by_year %>% filter(grepl("Pascal", x = player, ignore.case=TRUE), season == 2022)
+all_star_by_year %>% filter(grepl("Pascal", x = player, ignore.case=TRUE))
+all_league_not_all_stars %>% filter(grepl("Zach Randolph", player))
+
+## Data Visualizations ####
+## which positions?
+all_league_not_all_stars %>% 
+  ggplot(aes(position)) +
+  geom_bar() +
+  coord_flip()
+## which players?
+all_league_not_all_stars %>% 
+  count(player) %>% 
+  arrange(desc(n)) %>% #  nrow() # 58
+  filter(n > 1) %>%
+  ggplot(aes(reorder(player, n), n)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(breaks = 1:13, labels = 1:13) +
+  coord_flip() +
+  labs(x = "Player",
+       y = "Count")
+## spot check
+all_league_not_all_stars %>% filter(grepl("DeAndre Jordan", player, ignore.case=TRUE))
+all_league_long %>% filter(grepl("DeAndre Jordan", player, ignore.case=TRUE))
+all_star_by_year %>% filter(grepl("DeAndre Jordan", player, ignore.case=TRUE))
+
+## write to file ####
+openxlsx::write.xlsx(x = all_league_not_all_stars,file = here::here('data/all_nba_non_all_stars.xlsx'))
